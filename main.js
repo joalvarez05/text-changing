66865665f7c858d1cbd6df4a2212469cb547749c
Jose Alvarez Dominguez <josealvarezdominguez00@gmail.com>
2024-08-25 11:18:29 -0300

    Text-changing animation

---
+++ main.js
@@ -0,0 +1,33 @@
+var sliderCounter = 0;
+var sliderContent = ["GigaStore"];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+
+  sliderValue.classList.remove("holder-animation");
+  sliderValue.classList.add("holder-animation");
+
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
